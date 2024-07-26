37b22df360406651e6552045357b05fba350c1c5
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-07-26 13:27:31 +0200

    Update surefire and fix problem

--- src/main/java/com/basaki/controller/CustomErrorController.java
+++ src/main/java/com/basaki/controller/CustomErrorController.java
@@ -1,19 +1,24 @@
 package com.basaki.controller;
 
 import com.basaki.error.ErrorInfo;
+
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+
 import springfox.documentation.annotations.ApiIgnore;
 
 /**
@@ -25,45 +30,42 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @RestController
 @ApiIgnore
-@SuppressWarnings({"squid:S1075"})
+@SuppressWarnings({ "squid:S1075" })
 public class CustomErrorController implements ErrorController {
 
-    private static final Logger
-            log = LoggerFactory.getLogger(CustomErrorController.class);
+	private static final Logger log = LoggerFactory.getLogger(CustomErrorController.class);
 
-    private static final String PATH = "/error";
+	private static final String PATH = "/error";
 
-    @Value("${debug:true}")
-    private boolean debug;
+	@Value("${debug:true}")
+	private ErrorAttributeOptions debug;
 
-    private ErrorAttributes errorAttributes;
+	private final ErrorAttributes errorAttributes;
 
-    @Autowired
-    public CustomErrorController(ErrorAttributes errorAttributes) {
-        this.errorAttributes = errorAttributes;
-    }
+	@Autowired
+	public CustomErrorController(final ErrorAttributes errorAttributes) {
+		this.errorAttributes = errorAttributes;
+	}
 
-    @RequestMapping(value = PATH)
-    ErrorInfo error(HttpServletRequest request, HttpServletResponse response) {
-        ErrorInfo info = new ErrorInfo();
-        info.setCode(response.getStatus());
-        Map<String, Object> attributes = getErrorAttributes(request, debug);
-        info.setMessage((String) attributes.get("message"));
-        log.error((String) attributes.get("error"));
+	@RequestMapping(value = PATH)
+	ErrorInfo error(final HttpServletRequest request, final HttpServletResponse response) {
+		final ErrorInfo info = new ErrorInfo();
+		info.setCode(response.getStatus());
+		final Map<String, Object> attributes = getErrorAttributes(request, debug);
+		info.setMessage((String) attributes.get("message"));
+		log.error((String) attributes.get("error"));
 
-        return info;
-    }
+		return info;
+	}
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
-    }
+	public String getErrorPath() {
+		return PATH;
+	}
 
-    private Map<String, Object> getErrorAttributes(HttpServletRequest request,
-            boolean includeStackTrace) {
-        WebRequest webRequest =
-                new ServletWebRequest(request);
-        return errorAttributes.getErrorAttributes(webRequest,
-                includeStackTrace);
-    }
+	private Map<String, Object> getErrorAttributes(final HttpServletRequest request,
+			final ErrorAttributeOptions includeStackTrace) {
+		final WebRequest webRequest = new ServletWebRequest(request);
+		return errorAttributes.getErrorAttributes(webRequest,
+				includeStackTrace);
+	}
 }
--- src/test/java/com/basaki/controller/CustomErrorControllerTest.java
+++ src/test/java/com/basaki/controller/CustomErrorControllerTest.java
@@ -1,25 +1,27 @@
 package com.basaki.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.basaki.error.ErrorInfo;
+
 import java.util.HashMap;
 import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.WebRequest;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Mockito.when;
 
 /**
  * {@code CustomErrorControllerTest} represents unit test for {@code
@@ -31,38 +33,38 @@ import static org.mockito.Mockito.when;
  */
 public class CustomErrorControllerTest {
 
-    @Mock
-    private ErrorAttributes errorAttributes;
+	@Mock
+	private ErrorAttributes errorAttributes;
 
-    @InjectMocks
-    private CustomErrorController controller;
+	@InjectMocks
+	private CustomErrorController controller;
 
-    private MockHttpServletRequest request;
+	private MockHttpServletRequest request;
 
-    private MockHttpServletResponse response;
+	private MockHttpServletResponse response;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(
-                new ServletRequestAttributes(request));
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(
+				new ServletRequestAttributes(request));
 
-        response = new MockHttpServletResponse();
-    }
+		response = new MockHttpServletResponse();
+	}
 
-    @Test
-    public void testError() {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("message", "test-message");
-        attributes.put("error", "test-error");
+	@Test
+	public void testError() {
+		final Map<String, Object> attributes = new HashMap<>();
+		attributes.put("message", "test-message");
+		attributes.put("error", "test-error");
 
-        when(errorAttributes.getErrorAttributes(any(WebRequest.class),
-                anyBoolean())).thenReturn(attributes);
+		when(errorAttributes.getErrorAttributes(any(WebRequest.class),
+				any(ErrorAttributeOptions.class))).thenReturn(attributes);
 
-        ErrorInfo errorInfo = controller.error(request, response);
-        assertNotNull(errorInfo);
-        assertEquals("test-message", errorInfo.getMessage());
-        assertNotNull(controller.getErrorPath());
-    }
+		final ErrorInfo errorInfo = controller.error(request, response);
+		assertNotNull(errorInfo);
+		assertEquals("test-message", errorInfo.getMessage());
+		assertNotNull(controller.getErrorPath());
+	}
 }
